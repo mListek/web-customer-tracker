0c2e226f22847f413666446f723d31494ff2deab
mListek <michal.listex@gmail.com>
2020-07-19 11:01:34 +0200

    implemented customer service

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -2,9 +2,11 @@ package com.luv2code.springdemo.controller;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,15 +15,14 @@ import java.util.List;
 @RequestMapping("/customer")
 public class CustomerController {
 
-    // inject CustomerDAO
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model theModel) {
 
-        // get customers from the dao
-        List<Customer> theCustomers = customerDAO.getCustomers();
+        // get customers from the service
+        List<Customer> theCustomers = customerService.getCustomers();
 
         // add customers to the spring MVC model
         theModel.addAttribute("customers", theCustomers);
--- web/index.jsp
+++ web/index.jsp
@@ -1,16 +1,1 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: mlistek
-  Date: 17.07.2020
-  Time: 16:35
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
-</html>
+<% response.sendRedirect("customer/list"); %>
